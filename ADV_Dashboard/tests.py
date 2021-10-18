941bc8fade12e5c941f157d63b1b371fa12b5a46
allru <79753950+allru@users.noreply.github.com>
2021-10-18 18:23:42 +0200

    datenbereinigung und alles, was nicht kaputt ist

--- ADV_Dashboard/tests.py
+++ ADV_Dashboard/tests.py
@@ -2,15 +2,12 @@ import pandas as pd
 import plotly.express as px
 
 
-df = pd.read_csv('ErdbebenDaten.csv')
-
-df = pd.concat([df, df['place'].str.split(', ', expand=True)], axis=1)
-df.rename(columns={0: 'place', 1: 'state'}, inplace=True)
-
+df = pd.read_csv('clean_data.csv')
 
 
 fig = px.scatter_mapbox(df, lat="latitude", lon="longitude", hover_name="state", hover_data=["mag"],
-                        color_discrete_sequence=["fuchsia"], zoom=2.5, height=800)
+                        color_discrete_sequence=["fuchsia"], zoom=2.5, height=800, color='mag',
+                        size='mag', animation_frame="date")
 fig.update_layout(mapbox_style="open-street-map")
 #fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.show()
